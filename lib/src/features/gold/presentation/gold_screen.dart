b33b71145faf7741635b5f448aa77e39041642ad
mhmdgazzar <mhmdgazzar@gmail.com>
2024-06-24 12:07:27 +0200

    center gold stream

--- lib/src/features/gold/presentation/gold_screen.dart
+++ lib/src/features/gold/presentation/gold_screen.dart
@@ -1,36 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:stream_gold_rate/src/features/gold/data/fake_gold_api.dart';
 
 class GoldScreen extends StatelessWidget {
   const GoldScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    /// Platzhalter für den Goldpreis
-    /// soll durch den Stream `getGoldPriceStream()` ersetzt werden
-    const double goldPrice = 69.22;
-
     return SafeArea(
       child: Scaffold(
         body: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              _buildHeader(context),
-              const SizedBox(height: 20),
-              Text('Live Kurs:',
-                  style: Theme.of(context).textTheme.headlineMedium),
-              const SizedBox(height: 20),
-              // TODO: Verwende einen StreamBuilder, um den Goldpreis live anzuzeigen
-              // statt des konstanten Platzhalters
-              Text(
-                NumberFormat.simpleCurrency(locale: 'de_DE').format(goldPrice),
-                style: Theme.of(context)
-                    .textTheme
-                    .headlineLarge!
-                    .copyWith(color: Theme.of(context).colorScheme.primary),
-              ),
-            ],
+          child: Center(
+            child: Column(
+              children: [
+                _buildHeader(context),
+                const SizedBox(height: 24),
+                Text('Live Kurs:',
+                    style: Theme.of(context).textTheme.headlineLarge),
+                const SizedBox(height: 20),
+                StreamBuilder(
+                  stream: getGoldPriceStream(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData &&
+                            snapshot.connectionState == ConnectionState.done ||
+                        snapshot.connectionState == ConnectionState.active) {
+                      final response = snapshot.data;
+                      return Text(
+                        NumberFormat.simpleCurrency(locale: 'de_DE')
+                            .format(response),
+                        style: Theme.of(context)
+                            .textTheme
+                            .headlineLarge!
+                            .copyWith(
+                                color: Theme.of(context).colorScheme.primary),
+                      );
+                    } else if (snapshot.connectionState !=
+                        ConnectionState.done) {
+                      return const CircularProgressIndicator();
+                    } else {
+                      return const Icon(Icons.error);
+                    }
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -39,6 +52,7 @@ class GoldScreen extends StatelessWidget {
 
   Row _buildHeader(BuildContext context) {
     return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
         const Image(
           image: AssetImage('assets/bars.png'),
